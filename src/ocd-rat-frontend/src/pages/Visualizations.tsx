7df016f7615ead5bc2cbbfdbdf61fd1039a6f664
Timothy Pokanai <pokanait@mcmaster.ca>
2026-03-29 01:25:10 -0400

    Removed line charts for the time being

--- src/ocd-rat-frontend/src/pages/Visualizations.tsx
+++ src/ocd-rat-frontend/src/pages/Visualizations.tsx
@@ -3,7 +3,6 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import ChartImg from "@/assets/barchart.png";
-import LineChartImg from "@/assets/linechart.png";
 import MultiGraphImg from "@/assets/multigraph.png";
 import { Link } from 'react-router-dom';
 
@@ -98,12 +97,6 @@ export function Visualizations() {
       title: 'Bar Chart',
       desc: 'Categorical comparisons using configurable X and Y axes.',
       href: '/visualizations/bar-chart'
-    },
-    {
-      img: LineChartImg,
-      title: 'Line Chart',
-      desc: 'Time-series trends showing how metrics change over time.',
-      href: '/visualizations/line-chart'
     },
     {
       img: null,
